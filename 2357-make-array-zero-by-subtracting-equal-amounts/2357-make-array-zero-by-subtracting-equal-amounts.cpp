8053aac1173ffd397aba4194dd75f44dd67260cc
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-02-24 23:21:09 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (14.08%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -9,3 +9,36 @@ public:
         return st.size();
     }
 };
+// class Solution {
+// public:
+//     int c=0;
+//     int flag=0;
+//     int getmin(vector<int>& nums,int n){
+//         int m=INT_MAX;
+//         for(int i=0;i<n;i++){
+//             if(m>nums[i] && nums[i]!=0){
+//                 flag=1;
+//                 m=nums[i];
+//             }
+//             else if(nums[i]!=0 && m<nums[i])
+//                 flag=0;
+//         }
+//         return m;
+//     }
+//     void minumMin(vector<int>& nums,int mini,int n){
+//         for(int i=0;i<n;i++){
+//             if(nums[i]!=0){
+//                 nums[i]-=mini;
+//             }
+//         }
+//         if(flag)    c+=1;
+//     }
+//     int minimumOperations(vector<int>& nums) {
+//         int n=nums.size();
+//         for(int i=0;i<n;i++){
+//             int mini=getmin(nums,n);
+//             minumMin(nums,mini,n);
+//         }
+//         return c;
+//     }
+// };
