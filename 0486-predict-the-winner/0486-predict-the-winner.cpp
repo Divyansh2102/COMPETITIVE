1d6ed49a7d2043027e7b0dcbd83bcca169687b7a
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-02-27 19:04:32 +0530

    Time: 270 ms (11.16%), Space: 7.4 MB (88.24%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int check(vector<int>&nums,int s,int e){
+        if(s>e) return 0;
+        int a=nums[s]-check(nums,s+1,e);
+        int b=nums[e]-check(nums,s,e-1);
+        return max(a,b);
+    }
+    bool PredictTheWinner(vector<int>& nums) {
+        return check(nums,0,nums.size()-1)>=0;
+    }
+};
