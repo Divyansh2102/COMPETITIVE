65861caf4bfa127a5a04aafbf10b5970da2649f2
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-02-27 19:13:33 +0530

    Time: 6 ms (53.48%), Space: 7.7 MB (24.34%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-    int check(vector<int>&nums,int s,int e){
+    int check(vector<int>&nums,int s,int e,vector<vector<int>>&dp){
         if(s>e) return 0;
-        int a=nums[s]-check(nums,s+1,e);
-        int b=nums[e]-check(nums,s,e-1);
-        return max(a,b);
+        if(dp[s][e]!=-1)    return dp[s][e];
+        int a=nums[s]-check(nums,s+1,e,dp);
+        int b=nums[e]-check(nums,s,e-1,dp);
+        return dp[s][e]=max(a,b);
     }
     bool PredictTheWinner(vector<int>& nums) {
-        return check(nums,0,nums.size()-1)>=0;
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return check(nums,0,n-1,dp)>=0;
     }
 };
