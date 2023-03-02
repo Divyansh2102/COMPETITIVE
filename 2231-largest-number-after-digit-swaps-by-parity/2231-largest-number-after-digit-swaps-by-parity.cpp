4e58b782af8ce38f7b48e0242067576424285083
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-03-02 12:48:58 +0530

    Time: 2 ms (48.12%), Space: 6.2 MB (44.24%) - LeetHub

---
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int largestInteger(int num) {
+        priority_queue<int>peven;
+        priority_queue<int>podd;
+        int i=0;
+        string s=to_string(num);
+        int n=s.size();
+        while(n--){
+            if(s[i]%2==0)
+                peven.push(s[i]);
+            else
+                podd.push(s[i]);
+            i+=1;
+        }
+        i=0;
+        string ans;
+        while(!peven.empty() || !podd.empty()){
+            if(s[i]%2==0){
+                ans+=peven.top();
+                peven.pop();
+                
+            }
+            else{
+                ans+=podd.top();
+                podd.pop();
+            }
+            i++;
+        }
+        num=stoi(ans);
+        return num;
+    }
+};
+
+
