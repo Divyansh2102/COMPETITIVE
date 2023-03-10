f213451edb874dcf1829428b5868aa733a9b6982
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-03-10 23:11:59 +0530

    Time: 10 ms (26.59%), Space: 8.5 MB (24.28%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,8 +4,8 @@ public:
         vector<int>ans;
         for(int i=0;i<=n;i++){
             int x=i,c=0;
-            for(int j=0;x>0;j++){
-                if(x%2==1)  c+=1;
+            while(x!=0){
+                c+=x%2;
                 x/=2;
             }
             ans.push_back(c);
