873eb9aac8f0661a3636b4bb0ae5e85569a6d122
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-03-10 11:19:08 +0530

    Time: 28 ms (33.38%), Space: 16.8 MB (26.59%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.cpp
+++ 0382-linked-list-random-node/0382-linked-list-random-node.cpp
@@ -10,23 +10,17 @@
  */
 class Solution {
 public:
-    ListNode*temp;
+    vector<int>temp;
     Solution(ListNode* head) {
-        temp=head;
+        while(head){
+            temp.push_back(head->val);
+            head=head->next;
+        }
     }
     
     int getRandom() {
-        int ans=-1;
-        int i=1;
-        ListNode*x=temp;
-        while(x){
-            if(rand()%i==0){
-                ans=x->val;
-            }
-            i++;
-            x=x->next;
-        }
-        return ans;
+        int i=rand()%temp.size();
+        return temp[i];
     }
 };
 
