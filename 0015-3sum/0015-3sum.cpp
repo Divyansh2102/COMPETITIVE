3b30a768408a5a690eca606a909cf44339bf1e5d
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-03-14 22:16:06 +0530

    Time: 200 ms (56.82%), Space: 24 MB (83.71%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -10,11 +10,7 @@ public:
                 int lo=i+1,hi=n-1,sum=0-nums[i];
                 while(lo<hi){
                     if(nums[lo]+nums[hi]==sum){
-                        vector<int>tmp;
-                        tmp.push_back(nums[i]);
-                        tmp.push_back(nums[lo]);
-                        tmp.push_back(nums[hi]);
-                        ans.push_back(tmp);
+                        ans.push_back({nums[i],nums[lo],nums[hi]});
                         while(lo<hi && nums[lo]==nums[lo+1]) lo++;
                         while(lo<hi && nums[hi]==nums[hi-1]) hi--;
                         lo++,hi--;
