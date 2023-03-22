afb49923075125a8e78821d788ebece55e1968e4
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-03-22 22:08:48 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (73.13%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        vector<int>v(26,-1);
+        for(int i=0,k=0;i<key.size();i++){
+            if(key[i]!=' ' && v[key[i]-'a']==-1)
+                v[key[i]-'a']=k++;
+        }
+        for(int i=0;i<message.size();i++){
+            if(message[i]!=' ') message[i]=v[message[i]-'a']+'a';
+        }
+        return message;
+    }
+};
