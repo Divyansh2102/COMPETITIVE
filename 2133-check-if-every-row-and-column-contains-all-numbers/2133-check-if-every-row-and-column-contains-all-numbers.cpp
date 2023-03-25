f206b501b3e8c75e8dea3d064bc35a6fbe55334a
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-03-25 11:05:05 +0530

    Time: 119 ms (91.25%), Space: 35.3 MB (79.35%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     bool checkValid(vector<vector<int>>& m) {
         int n=m.size();
-        vector<vector<int>>v=m;
         for(int i=0;i<n;i++){
+            vector<bool>count(n+1);
             for(int j=0;j<n;j++){
-                v[i][j]=m[j][i];
+                auto num=m[i][j];
+                if(count[num])  return false;
+                count[num]=true;
             }
         }
-        for(int i=0;i<n;i++){
-            sort(m[i].begin(),m[i].end());
-            sort(v[i].begin(),v[i].end());
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(m[i][j]!=j+1)    return false;
-                if(v[i][j]!=j+1)    return false;
+        for(int j=0;j<n;j++){
+            vector<bool>count(n+1);
+            for(int i=0;i<n;i++){
+                auto num=m[i][j];
+                if(count[num])  return false;
+                count[num]=true;
             }
         }
         return true;
