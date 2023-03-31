ec742eaebd57b7e9d8df42e935f20735a9a3c8d8
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-03-31 12:21:11 +0530

    Time: 6 ms (47.74%), Space: 8.2 MB (12.08%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.cpp
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int oddCells(int m, int n, vector<vector<int>>& in) {
-        int a=0;
-        vector<vector<int>>ans(m,vector<int>(n,0));
-        for(int i=0;i<in.size();i++){
-                for(int k=in[i][0];k<=in[i][0];k++){
-                    for(int q=0;q<n;q++)    ans[k][q]+=1;
-                }
-                
-                for(int k=in[i][1];k<=in[i][1];k++){
-                    for(int q=0;q<m;q++)    ans[q][k]+=1;
-                } 
-            
+        int ans=0;
+        vector<vector<int>>v(m,vector<int>(n,0));
+        for(auto &i:in){
+            int temp=i[0];
+            for(int j=0;j<n;j++)    v[temp][j]+=1;
+            temp=i[1];
+            for(int j=0;j<m;j++)    v[j][temp]+=1;
         }
-        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                cout<<ans[i][j]<<"  ";
-                if(ans[i][j]%2!=0)  a+=1;
+                if(v[i][j]%2!=0)    ans+=1;
             }
-            cout<<endl;
         }
-        return a;
+        return ans;
     }
 };
