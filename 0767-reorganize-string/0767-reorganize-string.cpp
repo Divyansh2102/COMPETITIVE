9a09e6265d69d77d0bca279d8be616d06250e86b
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-04-04 19:40:03 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (56.84%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     string reorganizeString(string s) {
         unordered_map<char,int>mp;
-        for(auto &i:s)  mp[i]++;
+        for(auto &i:s)   mp[i]++;
         priority_queue<pair<int,char>>pq;
-        for(auto &i:mp) pq.push({i.second,i.first});
+        for(auto &i:mp){
+            pq.push({i.second,i.first});
+        }
         string ans="";
         while(pq.size()>1){
-            auto t1=pq.top();
+            auto v1=pq.top();
+            ans+=v1.second;
+            v1.first-=1;
             pq.pop();
-            auto t2=pq.top();
+            auto v2=pq.top();
+            ans+=v2.second;
+            v2.first-=1;
             pq.pop();
-            ans+=t1.second;
-            ans+=t2.second;
-            t1.first-=1;
-            t2.first-=1;
-            if(t1.first>0)  pq.push(t1);
-            if(t2.first>0)  pq.push(t2);
+            if(v1.first>0)  pq.push(v1);
+            if(v2.first>0)  pq.push(v2);
         }
-        if(!pq.empty()){
-            if(pq.top().first>1)    return "";
-            else ans+=pq.top().second;
+        if (!pq.empty()) {
+            if(pq.top().first > 1)    return "";
+            else    ans+=pq.top().second;
         }
         return ans;
     }
