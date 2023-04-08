bf82ba72f2f43ecaf89aeb9d58dff8cb7b336a89
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-04-08 11:42:45 +0530

    Time: 11 ms (85.27%), Space: 6 MB (63.29%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0) return false;
-        vector<int>temp;
-        while(x){
-            temp.push_back(x%10);
+        if(x<0 || x!=0 && x%10==0)  return false;
+        int flag=0;
+        while(x>flag){
+            flag=flag*10+x%10;
             x/=10;
         }
-        vector<int>temp1=temp;
-        reverse(temp1.begin(),temp1.end());
-        return temp1==temp;
+        return (x==flag || x==flag/10);
     }
 };
