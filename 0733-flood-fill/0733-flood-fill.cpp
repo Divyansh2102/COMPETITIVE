59f7d9e957acc7af8434ec97d886abc3d4115dae
Divyansh2102 <90768907+Divyansh2102@users.noreply.github.com>
2023-04-14 18:37:27 +0530

    Time: 10 ms (58.80%), Space: 14 MB (83.44%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    vector<int>dx={-1,0,0,1};
-    vector<int>dy={0,-1,1,0};
-    void ans(vector<vector<int>>&img,int sr,int sc,int r,int c,int ac,int color){
-        if(sr>=0 && sc>=0 && sr<r && sc<c && img[sr][sc]==ac){
-            img[sr][sc]=color;
-            for(int i=0;i<4;i++)
-                ans(img,sr+dx[i],sc+dy[i],r,c,ac,color);
-        }
+    void dfs(int i,int j,vector<vector<int>>&v,int inc,int nc){
+        int n=v.size();
+        int m=v[0].size();
+        if(i<0 || j<0)  return;
+        if(i>=n || j>=m)    return;
+        if(v[i][j]!=inc)    return;
+        v[i][j]=nc;
+        dfs(i-1,j,v,inc,nc);
+        dfs(i+1,j,v,inc,nc);
+        dfs(i,j-1,v,inc,nc);
+        dfs(i,j+1,v,inc,nc);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc]==color)    return image;
-        int r=image.size(),c=image[0].size();
-        ans(image,sr,sc,r,c,image[sr][sc],color);
+        int incolor=image[sr][sc];
+        if(incolor!=color)
+            dfs(sr,sc,image,incolor,color);
         return image;
     }
 };
