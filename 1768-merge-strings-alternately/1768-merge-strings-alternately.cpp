22e03b443b0fc3db6ab1be024f9ac1c892cd21d0
Divyansh Jaiswal <90768907+Divyansh2102@users.noreply.github.com>
2023-05-17 11:28:18 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (58.48%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     string mergeAlternately(string w1, string w2) {
-        string st="";
-        int n=w1.length(),m=w2.length();
-        int i=0,j=0,c=0;
-        while(i<n&&j<m){
-            if(c%2==0){
-                st+=w1[i++];
-                c++;
-            }
-            else{
-                st+=w2[j++];
-                c++;
-            }
+        string ans="";
+        int i=0,j=0;
+        while(i<w1.size() && j<w2.size()){
+            ans+=w1[i++];
+            ans+=w2[j++];
         }
-        while(i<n){
-                st+=w1[i++];
-            }
-        while(j<m){
-            st+=w2[j++];
+        if(i<w1.size()){
+            ans+=w1.substr(i);
         }
-        return st;
+        if(j<w2.size()){
+            ans+=w2.substr(j);
+        }
+        return ans;
     }
 };
