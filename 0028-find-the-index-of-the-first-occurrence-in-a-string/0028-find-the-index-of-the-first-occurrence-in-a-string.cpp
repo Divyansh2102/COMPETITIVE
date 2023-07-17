7e64de26de23454a920125c15c50316af43c9338
Divyansh Jaiswal <90768907+Divyansh2102@users.noreply.github.com>
2023-07-17 08:11:18 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (92.07%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int strStr(string s1, string s2) {
-        int n=s1.size(),m=s2.size();
-        if(m>n) return -1;
-        for(int i=0;i<n;i++){
-            if(s1[i]==s2[0]){
-                int j,k=i;
-                for(j=0;j<m;j++){
-                    if(s1[k]==s2[j])
-                        k++;
-                    else
-                        break;
+    int strStr(string x, string y) {
+        if(y.size()>x.size())   return -1;
+        for(int i=0;i<x.size();i++){
+            if(x[i]==y[0]){
+                int k=i,j;
+                for(j=0;j<y.size();j++){
+                    if(x[k]==y[j])k+=1;
+                    else break;
                 }
-                if(j==m)
-                    return i;
+                if(j==y.size()) return i;
             }
         }
         return -1;
