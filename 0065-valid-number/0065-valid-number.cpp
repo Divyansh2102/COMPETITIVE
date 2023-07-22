6cf61c7c071d9c704c75706e4672735ee3181a21
Divyansh Jaiswal <90768907+Divyansh2102@users.noreply.github.com>
2023-07-22 13:29:25 +0530

    Time: 4 ms (55.74%), Space: 9.4 MB (5.08%) - LeetHub

--- 0065-valid-number/0065-valid-number.cpp
+++ 0065-valid-number/0065-valid-number.cpp
@@ -1,28 +1,7 @@
 class Solution {
 public:
     bool isNumber(string s) {
-        int n=s.size();
-        bool digf=0,ef=0,decif=0;
-        int signC=0;
-        for(int i=0;i<n;i++){
-            if(isdigit(s[i]))   digf=1;
-            else if(s[i]=='+' || s[i]=='-'){
-                if(signC==2) return 0;
-                if(i>0 && (s[i-1]!='e' && s[i-1]!='E'))    return 0;
-                if(i==n-1)  return 0;
-                signC+=1;
-            }
-            else if(s[i]=='.'){
-                if(ef||decif)   return 0;
-                if(i==n-1 && !digf)    return 0;
-                decif=1;
-            }
-            else if(s[i]=='e' || s[i]=='E'){
-                if(ef||i==n-1||!digf)   return 0;
-                ef=1;
-            }
-            else return 0;
-        }
-        return 1;
+        static regex r(R"_([+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)_");
+        return regex_match(s,r);
     }
 };
