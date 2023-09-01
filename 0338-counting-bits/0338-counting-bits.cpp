353ed36f8763c9d1b7485cc97afc97307f9613df
Divyansh Jaiswal <90768907+Divyansh2102@users.noreply.github.com>
2023-09-01 09:48:29 +0530

    Time: 3 ms (89.82%), Space: 7.9 MB (51.17%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>ans;
-        for(int i=0;i<=n;i++){
-            int x=i,c=0;
-            while(x!=0){
-                c+=x%2;
-                x/=2;
-            }
-            ans.push_back(c);
+        vector<int>ans(n+1);
+        ans[0]=0;
+        for(int i=1;i<=n;i++){
+            ans[i]=ans[i/2]+i%2;
         }
         return ans;
     }
